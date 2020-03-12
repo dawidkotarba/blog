1c6609dfe67223cb57f215348b690c6002490953
dawidkotarba <dawidkotarba>
2020-03-12 15:29:52 +0100

    Update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
-git checkout gh-pages
-git merge master
-git push
-git checkout master
+# Whiptail configuration
+export NEWT_COLORS='root=,black
+    roottext=red,white
+    entry=red,white'
+SETUP_WINDOW_TITLE="Script to push to gh-pages"
+WINDOW_HEIGHT=10
+WINDOW_WIDTH=100
+
+function show_yesno_box(){
+    whiptail --title "Conditional action:" --backtitle "${SETUP_WINDOW_TITLE}" --yesno "$1" ${WINDOW_HEIGHT} ${WINDOW_WIDTH}
+}
+
+if show_yesno_box "Push to github pages?"; then
+    git checkout gh-pages
+    git merge master
+    git push
+    git checkout master
+fi
+
